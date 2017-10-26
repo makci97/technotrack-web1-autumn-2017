15932ccfc0364e8d20d0bab15487e2ef31c5ce70
makci97 <makci.97@mail.ru>
2017-10-26 18:09:59 +0300

    Add sign up, login and logout

--- project/application/settings.py
+++ project/application/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+from django.conf.global_settings import LOGIN_URL, LOGIN_REDIRECT_URL
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -129,3 +131,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_URL = 'core:login'
+LOGIN_REDIRECT_URL = 'core:index'
--- project/core/urls.py
+++ project/core/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import url
 from core import views
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^login/$', LoginView.as_view(template_name='core/login.html'), name='login'),
+    url(r'^logout/$', LogoutView.as_view(template_name='core/logout.html'), name='logout'),
+    url(r'^sign_up/$', views.sign_up, name='sign_up'),
 ]
