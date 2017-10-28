38f6a2b75c34dc7a307420c990dd1b85669f0705
makci97 <makci.97@mail.ru>
2017-10-28 19:45:32 +0300

    Add pagination of blog_list and post_list for current blog(but BlogDetail(ListView): model=post ...)

--- project/blog/views.py
+++ project/blog/views.py
@@ -2,22 +2,58 @@ from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView
 
 from blog.models import Blog
+from post.models import Post
 
 
 class BlogList(ListView):
+    model = Blog
     template_name = "blog/blogs_list.html"
     context_object_name = 'blogs'
-    model = Blog
+    paginate_by = 1
 
 
-class BlogDetail(DetailView):
+class BlogDetail(ListView):
+    model = Post
     template_name = "blog/blog_page.html"
-    context_object_name = 'blog'
-    model = Blog
+    context_object_name = 'posts'
+    paginate_by = 2
+
+    def dispatch(self, request, *args, **kwargs):
+        self.blog = get_object_or_404(Blog.objects.all(), id=kwargs.get('pk'))
+        return super(BlogDetail, self).dispatch(request, *args, **kwargs)
 
     def get_context_data(self, **kwargs):
         context = super(BlogDetail, self).get_context_data(**kwargs)
+        context['blog'] = self.blog
         context['can_create'] = (
-            self.request.user.id == self.object.author.id
+            self.request.user.id == self.blog.author.id
         )
         return context
+
+    def get_queryset(self):
+        queryset = super(BlogDetail, self).get_queryset()
+        return queryset.filter(blog=self.blog)
+
+
+# class BlogDetail(DetailView):
+#     model = Blog
+#     template_name = "blog/blog_page.html"
+#     context_object_name = 'blog'
+#     paginate_by = 2
+#
+#     def get_context_data(self, **kwargs):
+#         context = super(BlogDetail, self).get_context_data(**kwargs)
+#         context['can_create'] = (
+#             self.request.user.id == self.object.author.id
+#         )
+#         return context
+#
+#     def get_object(self, queryset=None):
+#         if queryset is None:
+#             queryset = super(BlogDetail, self).get_queryset()
+#         return super(BlogDetail, self).get_object(queryset)
+#
+#     def get_queryset(self):
+#         # return Post.objects.all()
+#         print(self.object)
+#         return self.object.posts.all()
